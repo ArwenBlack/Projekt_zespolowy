e9423908aa6a7da5d782e025b50073ea437f60d4
Alicja Gulaszewska <alicja.gulaszewska@gmail.com>
2021-05-17 23:29:02 +0200

    Finished frontend from applying perspective

--- Projekt_zespołowy/settings.py
+++ Projekt_zespołowy/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,7 +82,7 @@ WSGI_APPLICATION = 'Projekt_zespołowy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
     }
 }
 
