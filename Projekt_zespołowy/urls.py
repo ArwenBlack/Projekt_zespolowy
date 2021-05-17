f31d8d6f3d63d296f069048192be33a2f5a11626
ArwenBlack <76584672+ArwenBlack@users.noreply.github.com>
2021-05-18 01:24:30 +0200

    Update, coś się przeklikuje

--- Projekt_zespołowy/urls.py
+++ Projekt_zespołowy/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', viewstart.main_page, name="main_page"),
     # url do testowania widoku szczegółów oferty
-    path('offer/', viewstart.offer_page, name="offer_page")
+    path('offer/<str:offer_title>/', viewstart.offer_page, name="offer_page"),
+    path('offer_test/', viewstart.offer_page_test, name="offer_page"),
+    path('<str:tag>/filtered/', viewstart.main_page, name= "filtered_main_page")
 ]
