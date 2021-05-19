508b3b90aef1eb0518956633f0f62014b7686d63
ArwenBlack <76584672+ArwenBlack@users.noreply.github.com>
2021-05-19 09:23:59 +0200

    naprawa

--- Projekt_zespołowy/settings.py
+++ Projekt_zespołowy/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'Projekt_zespołowy'
+    'crispy_forms'
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
