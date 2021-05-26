dd5a9dbbbae6029627ddf51ff9c3493c11dc068f
gawronCode <gawroncode@gmail.com>
2021-05-26 17:19:31 +0200

    dodanie klasy wysyłającej maile, przeglądanie realnych ofert z bazy danych

--- Projekt_zespołowy/urls.py
+++ Projekt_zespołowy/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from Views import viewstart
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', viewstart.main_page, name="main_page"),
@@ -9,3 +10,4 @@ urlpatterns = [
     path('offer/<str:offer_title>/', viewstart.offer_page, name="offer_page"),
     # path('offer_test/', viewstart.offer_page_test, name="offer_page"),
 ]
+
